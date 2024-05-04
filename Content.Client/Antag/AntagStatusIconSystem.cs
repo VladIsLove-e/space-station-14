4f336758a8474f4f2d187780cc4a048f09581a8e
Dexler <69513582+DexlerXD@users.noreply.github.com>
2024-05-04 20:54:39 +0300

    fixed role icons display

--- Content.Client/Antag/AntagStatusIconSystem.cs
+++ Content.Client/Antag/AntagStatusIconSystem.cs
@@ -1,5 +1,6 @@
 using Content.Shared.Antag;
 using Content.Shared.Revolutionary.Components;
+using Content.Shared.SS220.AdmemeEvents;
 using Content.Shared.StatusIcon;
 using Content.Shared.StatusIcon.Components;
 using Content.Shared.Zombies;
@@ -22,6 +23,7 @@ public sealed class AntagStatusIconSystem : SharedStatusIconSystem
         SubscribeLocalEvent<RevolutionaryComponent, GetStatusIconsEvent>(GetRevIcon);
         SubscribeLocalEvent<ZombieComponent, GetStatusIconsEvent>(GetIcon);
         SubscribeLocalEvent<HeadRevolutionaryComponent, GetStatusIconsEvent>(GetIcon);
+        SubscribeLocalEvent<EventRoleComponent, GetStatusIconsEvent>(GetIcon); //SS220-admeme-ebents
     }
 
     /// <summary>
